12d22d269cf8eff285b583a2878b0451aa580635
emilieyyu <emilieyyu@hotmail.com>
2018-11-07 23:00:26 -0800

    added profile

--- js/main.js
+++ js/main.js
@@ -60,13 +60,13 @@
         }
         , messages: {
             user_name: {
-                required: "Come on, you have a name don't you?", minlength: "Your name must consist of at least 2 characters"
+                required: "Please enter your name.", minlength: "Minimum of at least 2 characters"
             }
             , email: {
-                required: "Please put your email address",
+                required: "Please enter your email address",
             }
             , message: {
-                required: "Put some messages here?", minlength: "Your name must consist of at least 2 characters"
+                required: "Please enter your message", minlength: "Minimum of at least 2 characters"
             }
             ,
         }
@@ -115,7 +115,7 @@ $(document).ready(function(){
     });
 
     $("#clients-logo").owlCarousel({
- 
+
         itemsCustom : false,
         pagination : false,
         items : 5,
@@ -140,8 +140,8 @@ $(".fancybox").fancybox({
 
     closeClick : true,
     helpers : {
-        title : { 
-            type: 'inside' 
+        title : {
+            type: 'inside'
         },
         overlay : {
             css : {
@@ -150,14 +150,3 @@ $(".fancybox").fancybox({
         }
     }
 });
-
-
-
-
-
-
- 
-
-
-
-
